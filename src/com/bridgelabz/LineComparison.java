974e9a64378615f3212b11ce5d8f2fa98e184065
Your Name <you@example.com>
2022-11-11 10:54:27 +0530

    Added: Comparint two lines based on end point

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -8,30 +8,36 @@ public class LineComparison {
 
         System.out.println("Welcome to line comparison program");
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter point x1 and y1: ");
-        int x1 = sc.nextInt();
-        int y1 = sc.nextInt();
-        System.out.println("Enter point x2 and y2: ");
-        int x2 = sc.nextInt();
-        int y2 = sc.nextInt();
-        Double length1 = Math.sqrt((Math.pow((x2 - x1), 2)) + (Math.pow((y2 - y1), 2)));
-        System.out.println("Length of first line: " + length1);
-        System.out.println("Enter point x3 and y3: ");
-        int x3 = sc.nextInt();
-        int y3 = sc.nextInt();
-        System.out.println("Enter point x4 and y4: ");
-        int x4 = sc.nextInt();
-        int y4 = sc.nextInt();
-        Double length2 = Math.sqrt((Math.pow((x4 - x3), 2)) + (Math.pow((y4 - y3), 2)));
-        System.out.println("Length of second line is: " + length2);
-
-        boolean result = length2.equals(length1);
-
-        if (result == true) {
-            System.out.println("Lines are equal");
-        } else {
-            System.out.println("Lines are not equal");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter x1 and y1");
+        int x1 = scanner.nextInt();
+        int y1 = scanner.nextInt();
+        System.out.println("the values of (x1,x2) ;" + x1 + "," + y1);
+        System.out.println("enter x2 and y2");
+        int x2 = scanner.nextInt();
+        int y2 = scanner.nextInt();
+        System.out.println("the values of (x2,y2) ;" + x2 + "," + y2);
+        double length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        System.out.println("length of line :" + length);
+        double length1 = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        System.out.println("length of line :" + length1);
+        System.out.println("enter x3 and y3");
+        int x3 = scanner.nextInt();
+        int y3 = scanner.nextInt();
+        System.out.println("the values of (x3,y3) ;" + x3 + "," + y3);
+        System.out.println("enter x4 and y4");
+        int x4 = scanner.nextInt();
+        int y4 = scanner.nextInt();
+        System.out.println("the values of (x4,y4) ;" + x4 + "," + y4);
+        double length2 = Math.sqrt(Math.pow(x4 - x3, 2) + Math.pow(y4 - y3, 2));
+        System.out.println("length of line :" + length2);
+        System.out.println("length of lines are : ");
+        System.out.println("line1:" + length1);
+        System.out.println("line2:" + length2);
+        if (length1 > length2) {
+            System.out.println("line1 is greater than line2");
+        } else if (length1 < length2) {
+            System.out.println("line1 is less than line2");
         }
     }
 }
